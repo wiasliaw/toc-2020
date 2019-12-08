ad54db10a35c87c01f4c9f7935c6fa6a1519e9c5
Wei Lee <leewei48750@gmail.com>
2019-12-08 19:01:19 +0800

    feat(./): feat chatbot

--- packages/server/src/router/index.ts
+++ packages/server/src/router/index.ts
@@ -1,10 +1,14 @@
+// import koa from 'koa';
 import koaRouter from 'koa-router';
 import testHandler from '../lib/testHandler';
-import botHandler from '../lib/botHandler';
+// import botHandler from '../lib/botHandler';
 
 const router = new koaRouter();
 
 router.get('/test', testHandler);
-// router.post('/webhook', );
+// router.post('/webhook', async (ctx: koa.Context) => {
+//   await botHandler(ctx.req, ctx.res);
+//   ctx.respond = false;
+// });
 
 export default router;
